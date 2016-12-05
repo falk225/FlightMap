79d81863d36f222e2a8ba571bac98724a669af8a
falk225 <falk225@gmail.com>
2016-12-04 22:59:54 -0500

    12/4/2016

--- flight_map.js
+++ flight_map.js
@@ -36,30 +36,70 @@ function draw_map(geo_data) {
         .style('stroke', 'black')
         .style('stroke-width', 0.5);
 
-    function plot_airports(flight_data) {
-        //filters to continental US plus alasaka and hawaii
-        var filtered = flight_data.filter(function(d) {
-                        return (d.OrigLong>-172 &&
-                            d.OrigLong <-63 &&
-                            d.OrigLat>19 &&
-                            d.OrigLat<72);
-                    });
+    function populate_map(flight_data) {
 
-        svg.append('g')
-            .attr('class','airports')
-            .selectAll('circle')
-            .data(filtered)
-            //.data(flight_data)
-            .enter()
-            .append('circle')
-            .attr('r',5)
-            .attr('cx', function(d){
-                return projection([d.OrigLong, d.OrigLat])[0];
-            })
-            .attr('cy', function(d){
-                return projection([d.OrigLong, d.OrigLat])[1];
+        function plot_airports(airport_data) {
+            //filters to continental US plus alasaka and hawaii
+            var filtered = airport_data.filter(function(d) {
+                            return (d.values.long > -172 &&
+                                d.values.long < -63 &&
+                                d.values.lat > 19 &&
+                                d.values.lat < 72);
             });
+
+            svg.append('g')
+                .attr('class','airports')
+                .selectAll('circle')
+                .data(filtered)
+                //.data(flight_data)
+                .enter()
+                .append('circle')
+                .attr('r',5)
+                .attr('cx', function(d){
+                    return projection([d.values.long, d.values.lat])[0];
+                })
+                .attr('cy', function(d){
+                    return projection([d.values.long, d.values.lat])[1];
+                });
+        };
+        /*
+        var ap_codes = [];
+        var airports=[];
+
+        flight_data.forEach(function(d) {
+            if(ap_codes.includes(d.Origin)=== false){
+                ap_codes.push(d.Origin);
+                airports.push([d.Origin, d.OrigLong, d.OrigLat]);
+            };
+            if(ap_codes.includes(d.Dest)=== false){
+                ap_codes.push(d.Dest);
+                airports.push([d.Dest, d.DestLong, d.DestLat]);
+            };
+        });
+*/
+        function groupby_orig(data){
+            var n = d3.sum(data,function(d){
+                            return d['n'];
+                        });
+            var long = data[0].OrigLong;
+            var lat = data[0].OrigLat;
+            return {
+                    'n' : n,
+                    'long' : long,
+                    'lat' : lat
+                    };
+        }
+
+        var orig_airports = d3.nest()
+                    .key(function(d) {
+                        return d.Origin;
+                    })
+                    .rollup(groupby_orig)
+                    .entries(flight_data);
+        //console.log(orig_airports[0]);
+
+        plot_airports(orig_airports);
     }
 
-    d3.csv("flight_data.csv", plot_airports);
+    d3.csv("flight_data.csv", populate_map);
 }
